aae0cc15c30cd48e4de48a6409610d4722d8d45b
Arif Ebrahim <>
2021-05-12 10:18:49 +0100

    first test complete

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+
 class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    'Hello Battle!'
+    'Testing infrastructure working!'
   end
 
   # start the server if ruby file executed directly
